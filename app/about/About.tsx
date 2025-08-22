2be814a4f7531e18d607473bd23993475d072dbe
Ricky Tran <rickie.tran7@gmail.com>
2025-08-21 23:09:10 -0400

    removed comments

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -130,7 +130,7 @@ export default function About() {
         <div className="text-2xl">Safety First</div>
         <div className="flex flex-col w-full gap-4">
           <Section
-            topic={TOPICS.VENT_OR_FILT}
+            topic={TOPICS.FENT_OR_VENT}
             title="Ventilation vs. Filtration"
           >
             <div className="pt-4 flex flex-col gap-4">
@@ -171,7 +171,7 @@ export default function About() {
         <div className="text-2xl">That's It!</div>
         <div className="flex flex-col w-full gap-4">
           <Section topic={TOPICS.SOFTWARE} title="Start Printing!">
-            {CONSTANTS.FINALE /* make sure this exists */}
+            {CONSTANTS.FINALE}
           </Section>
         </div>
 
