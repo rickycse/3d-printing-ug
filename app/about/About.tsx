9755fbaaeccaafac7473fcf0ff79068399791e3f
Ricky Tran <rickie.tran7@gmail.com>
2025-08-24 00:19:47 -0400

    added scotts video

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -112,7 +112,17 @@ export default function About() {
 
         <div className="text-2xl">Optional Read</div>
         <Section topic={TOPICS.YAPPING} title="My Yapping Session" color="blue">
-          <div className="whitespace-pre-line">{CONSTANTS.YAPPING}</div>
+          <div className="pt-4 flex flex-col gap-4 whitespace-pre-line">
+            <div>
+              {CONSTANTS.YAPPING}
+            </div>
+            <div>
+              If you somehow haven't seen any of Scott Yu-Jan's videos, here's the one that got me into printing:
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center">
+              <VideoComponent src={"https://www.youtube.com/embed/9FHjfzS4BqQ"} />
+            </div>
+          </div>
         </Section>
 
         <div className="text-2xl">The Basics</div>
